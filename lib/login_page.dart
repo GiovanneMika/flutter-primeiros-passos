1df03d2965d119fa1af5cd1b80a26f77b14f28e0
Giovanne-Vamilly <mika.2023@alunos.utfpr.edu.br>
2026-01-13 10:24:59 -0300

    refactor: tela de login refatorada

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hello_world/home_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,72 +12,157 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  height: 200,
-                  width: 200,
-                  child: Image.asset('assets/images/logo.png'),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  onChanged: (text) {
-                    setState(() {
-                      email = text;
-                    });
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    label: Text("Email"),
-                    border: OutlineInputBorder(),
-                    errorText: email.isEmpty || email.contains("@")
-                        ? null
-                        : "Insira um email válido",
+
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "CELLIER",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'serif',
+                    ),
                   ),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    setState(() {
-                      password = text;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    label: Text("Password"),
-                    border: OutlineInputBorder(),
-                    errorText: password.isEmpty || password.length >= 5
-                        ? null
-                        : "A senha deve ter pelo menos 5 caracteres",
+                  SizedBox(height: 20),
+                  Container(
+                    height: 200,
+                    width: 200,
+                    child: Image.asset('assets/images/logo.png'),
                   ),
-                  obscureText: true,
-                ),
-                SizedBox(height: 15),
-                ElevatedButton(
-                  onPressed: () {
-                    if (email == "admin@email.com" && password == "12345") {
-                      print("correto");
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print("Login inválido");
-                    }
-                  },
-                  child: Text("Entrar"),
-                ),
-              ],
+                  SizedBox(height: 40),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      color: const Color.fromARGB(45, 0, 0, 0),
+                      child: Column(
+                        children: [
+                          TextField(
+                            onChanged: (text) {
+                              setState(() {
+                                email = text;
+                              });
+                            },
+                            style: TextStyle(color: Colors.white),
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              label: Text(
+                                "Email",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                  width: 2,
+                                ),
+                              ),
+                              errorText: email.isEmpty || email.contains("@")
+                                  ? null
+                                  : "Insira um email válido",
+                              filled: true,
+                              fillColor: const Color.fromARGB(55, 0, 0, 0),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          TextField(
+                            onChanged: (text) {
+                              setState(() {
+                                password = text;
+                              });
+                            },
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              label: Text(
+                                "Password",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                  width: 2,
+                                ),
+                              ),
+                              errorText:
+                                  password.isEmpty || password.length >= 5
+                                  ? null
+                                  : "A senha deve ter pelo menos 5 caracteres",
+                              filled: true,
+                              fillColor: const Color.fromARGB(55, 0, 0, 0),
+                            ),
+                            obscureText: true,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (email == "admin@email.com" &&
+                              password == "12345") {
+                            print("correto");
+                            Navigator.of(context).pushReplacementNamed('/home');
+                          } else {
+                            print("Login inválido");
+                          }
+                        },
+                        child: Text(
+                          "Entrar",
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Image.asset(
+              'assets/images/background.jpg',
+              fit: BoxFit.fill,
+            ),
+          ),
+          _body(),
+        ],
       ),
     );
   }
